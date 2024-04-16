819f5c571e3452fff8c9579234c10f5ea302d410
Darnell <darnellgarcia26@gmail.com>
2024-04-15 21:23:38 -0400

    Fixing react router client side routing on Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import ssr from 'vite-plugin-ssr/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    reactRefresh(),
+    ssr(),
+  ],
 })
