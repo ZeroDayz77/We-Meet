23498b31b40449df4f1a09338ae3487350ed7bdc
Darnell <darnellgarcia26@gmail.com>
2024-04-15 21:25:49 -0400

    Undid config changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import ssr from 'vite-plugin-ssr/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    reactRefresh(),
-    ssr(),
-  ],
+  plugins: [react()],
 })
